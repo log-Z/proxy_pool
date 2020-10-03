abb550ba34e864209eca56c24a71c2fbbe0b0a2d
log-Z <535065234@qq.com>
2020-10-03 18:52:28 +0800

    加入MySQL查询

--- database.py
+++ database.py
@@ -4,15 +4,16 @@ import pymysql
 
 class MySQLOperation:
     @staticmethod
-    def insert(entity:Model):
+    def insert(entity:Model) -> bool:
         table = MySQLOperation.__table_name(entity)
         fields = MySQLOperation.__fields_substament(entity)
         values = MySQLOperation.__values_substament(entity)
 
-        MySQLOperation.__execute(f'insert into {table}({fields}) \nvalues ({values});')
+        row_num = MySQLOperation.__execute(f'insert into {table}({fields}) \nvalues ({values});')
+        return row_num > 0
 
     @staticmethod
-    def batch_insert(entity_list:list):
+    def batch_insert(entity_list:list) -> int:
         if entity_list is None or len(entity_list) == 0:
             return
         table = MySQLOperation.__table_name(entity_list[0])
@@ -20,15 +21,46 @@ class MySQLOperation:
         values_list = [MySQLOperation.__values_substament(entity) for entity in entity_list]
         mult_values = '\n, '.join(map(lambda values: '({})'.format(values), values_list))
         
-        MySQLOperation.__execute(f'insert into {table}({fields}) \nvalues {mult_values};')
+        return MySQLOperation.__execute(f'insert into {table}({fields}) \nvalues {mult_values};')
 
     @staticmethod
-    def __execute(sql:str):
+    def select(entity:Model) -> list:
+        table = MySQLOperation.__table_name(entity)
+        field_names = entity._metadata.keys()
+        fields = MySQLOperation.__fields_substament(entity)
+
+        result = []
+        data = MySQLOperation.__query(f'select {fields} from {table};')
+        for row in data:
+            obj = entity.__class__()
+            for field, value in zip(field_names, row):
+                if field in field_names:
+                    setattr(obj, field, value)
+            result.append(obj)
+        return result
+
+    @staticmethod
+    def __execute(sql:str) -> int:
+        row_num = 0
+        connect = pymysql.connect(**Config.database)
+        with connect.cursor() as cursor:
+            row_num = cursor.execute(sql)
+            connect.commit()
+        connect.close()
+        return row_num
+
+    @staticmethod
+    def __query(sql:str) -> list:
+        result = []
         connect = pymysql.connect(**Config.database)
         with connect.cursor() as cursor:
             cursor.execute(sql)
-            connect.commit()
+            data = cursor.fetchall()
+            if data:
+                result = data
+
         connect.close()
+        return result
 
     @staticmethod
     def __table_name(entity:Model) -> str:
